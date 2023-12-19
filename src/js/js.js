72da1e1789efa9f72efa3840a1c82ac97e1386f9
Niyat Debesay <niyatdebesay26@gmail.com>
2023-12-19 11:07:41 +0300

    html-css-javascript

--- src/js/js.js
+++ src/js/js.js
@@ -7,26 +7,4 @@ container.addEventListener("mousemove" , (e)=>{
     cards.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
     console.log(e.pageX, e.pageY)
 });
-const text = document.querySelector("h2");
-const strText = text.textContent;
-const splittext = strText.split("")
-text.textContent = ""
-console.log(splittext);
-for (let i = 0 ; i < splittext.length;i++){
-    text.innerHTML+="<span>"+ splittext[i] + "</span>"
-}
-let char = 0;
-let timer = setInterval(onTick,50);
-function onTick(){
-    const span = text.querySelectorAll("span"[char])
-    span.classList.add('fade');
-    char++
-}
-if (char ===splittext.length){
-    complete();
-    return;
-}
-function complete(){
-    clearInterval(timer);
-    timer = null;
-}
+
